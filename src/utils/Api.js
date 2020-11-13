f91503f5661917503c76d3e8e8dd3d29a11f36ea
Code Boy <stefanjohnson650@gmail.com>
2020-11-12 19:38:04 -0800

    deleted api folder

--- src/utils/Api.js
+++
@@ -1,9 +0,0 @@
-// Test
-import axios from "axios";
-// eslint-disable-next-line
-export default {
-  // Gets all users
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
-};
